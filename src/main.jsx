a00d7f5f17dfac56f55a502bd4bf89b61552b6c1
FloriRox <pilzwegerflorian@gmail.com>
2026-06-07 13:31:06 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App, { DisplayApp } from './App.jsx'
+
+const isDisplay = new URLSearchParams(location.search).get('mode') === 'display';
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode>{isDisplay ? <DisplayApp/> : <App/>}</StrictMode>
 )
